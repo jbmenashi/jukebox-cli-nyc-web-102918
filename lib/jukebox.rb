4f6ee62ef4c6934d0be1c0f289db53fef87032df
Jacob Menashi <jbmenashi@gmail.com>
2018-10-15 22:07:19 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,6 +14,7 @@ def help
   puts 
   I accept the following commands:
   - help : displays this help message
-- list : displays a list of songs you can play
-- play : lets you choose a song to play
-- exit : exits this program
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program
+end
