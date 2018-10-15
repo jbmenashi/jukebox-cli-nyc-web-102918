fc43c8d776479cf592c1d45c7753f7ef60c5df34
Jacob Menashi <jbmenashi@gmail.com>
2018-10-15 22:55:34 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,16 +20,8 @@ def help
 end
 
 def list(songs)
-  puts "1. Phoenix - 1901
-        2. Tokyo Police Club - Wait Up
-        3. Sufjan Stevens - Too Much
-        4. The Naked and the Famous - Young Blood
-        5. (Far From) Home - Tiga
-        6. The Cults - Abducted
-        7. Phoenix - Consolation Prizes
-        8. Harry Chapin - Cats in the Cradle
-        9. Amos Lee - Keep It Loose, Keep It Tight"
-end
+  songs.each_with_index do |song, number|
+    puts "#{number "
 
 def play(songs)
   puts "Please enter a song name or number"
